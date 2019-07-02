bf3737166ce8297fb3de6b9798357eeb5b93303f
SZIUABP.PTE <szalai.istvan@pte.hu>
2019-07-02 13:50:02 +0200

    create init

--- Module.php
+++ Module.php
@@ -2,9 +2,23 @@
 
 namespace istvan0304\imagemanager;
 
+use Yii;
 use yii\base\Module as BaseModule;
 
 class Module extends BaseModule
 {
+    public $defaultRoute = 'ck-image';
 
+    public function init()
+    {
+        parent::init();
+
+        if (!isset(Yii::$app->i18n->translations['ckimage'])) {
+            Yii::$app->i18n->translations['ckimage'] = [
+                'class' => 'yii\i18n\PhpMessageSource',
+                'sourceLanguage' => 'en',
+                'basePath' => '@istvan0304/imagemanager/messages'
+            ];
+        }
+    }
 }
