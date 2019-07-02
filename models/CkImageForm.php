301bdb248fbdeb1630d0ceebd9038c24f52b5332
SZIUABP.PTE <szalai.istvan@pte.hu>
2019-07-02 14:58:17 +0200

    delete upload method

--- models/CkImageForm.php
+++ models/CkImageForm.php
@@ -10,22 +10,13 @@ class CkImageForm extends Model
 {
     public $img_files;
 
+    /**
+     * @return array
+     */
     public function rules()
     {
         return [
             [['img_files'], 'file', 'extensions' => 'jpg, jpeg, png', 'maxSize' => 3000000, 'maxFiles' => 10],
         ];
-    }
-
-    public function upload()
-    {
-        if ($this->validate()) {
-            foreach ($this->img_file as $file) {
-                $file->saveAs('uploads/' . $file->baseName . '.' . $file->extension);
-            }
-            return true;
-        } else {
-            return false;
-        }
     }
 }
