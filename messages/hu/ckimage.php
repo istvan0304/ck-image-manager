3300de7ba932b819c7c477b41cccec12b21b326a
SZIUABP.PTE <szalai.istvan@pte.hu>
2019-07-24 14:32:26 +0200

    translations

---
+++ messages/hu/ckimage.php
@@ -0,0 +1,10 @@
+<?php
+return [
+    'Delete' => 'Törlés',
+    'Details' => 'Részletek',
+    'File has been uploaded successfully!' => 'Fájl sikeresen feltöltve! ',
+    'No image selected.' => 'Nincs kiválasztva fájl.',
+    'Select' => 'Kiválasztás',
+    'Upload' => 'Feltöltés',
+    'Uploaded: ' => 'Feltöltve: '
+];
