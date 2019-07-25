9e32e6f69d62c52bdd49734672ff5e4045778c1d
SZIUABP.PTE <szalai.istvan@pte.hu>
2019-07-25 11:04:16 +0200

    translations

--- messages/hu/ckimage.php
+++ messages/hu/ckimage.php
@@ -5,6 +5,7 @@ return [
     'File has been uploaded successfully!' => 'Fájl sikeresen feltöltve! ',
     'No image selected.' => 'Nincs kiválasztva fájl.',
     'Select' => 'Kiválasztás',
+    'This file is already uploaded!' => 'Ezt a fájlt már feltöltötte!',
     'Upload' => 'Feltöltés',
     'Uploaded: ' => 'Feltöltve: '
 ];
