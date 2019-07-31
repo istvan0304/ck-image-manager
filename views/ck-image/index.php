fea45540f072d3f3e36419063df0a9c8c486aaef
SZIUABP.PTE <szalai.istvan@pte.hu>
2019-07-31 14:22:22 +0200

    loader

--- views/ck-image/index.php
+++ views/ck-image/index.php
@@ -2,9 +2,10 @@
 
 /* @var $this \yii\web\View */
 /* @var $ckImageManagerForm istvan0304\imagemanager\models\CkImageForm */
+
 /* @var $ckImages */
 
-?>
+use yii\widgets\Pjax; ?>
 
 <div class="ck-details">
     <div class="ck-details-header">
@@ -25,15 +26,26 @@
         <div class="ck-progress-line">
             <div class="ck-progress"><span id="ck-percentage"></span></div>
         </div>
-        <button id="ck-upload-details" class="ck-btn ck-btn-second"><i class="fas fa-info-circle"></i> <?= Yii::t('ckimage', 'Details') ?></button>
+        <button id="ck-upload-details" class="ck-btn ck-btn-second"><i
+                    class="fas fa-info-circle"></i> <?= Yii::t('ckimage', 'Details') ?></button>
     </div>
 
 </div>
 
+<?php Pjax::begin(['id' => 'ck-pjax-image-list', 'options' => ['class' => 'ck-image-list']]); ?>
 <?php echo $this->render('_imageList',
     [
         'ckImages' => $ckImages
     ]); ?>
+
+<?php Pjax::end(); ?>
+
+<div id="ck-list-loader" class="justify-content-center">
+    <div class="ck-loader-blur"></div>
+    <div class="ck-spinner-border text-primary" role="status">
+        <span class="sr-only">Loading...</span>
+    </div>
+</div>
 
 <aside id="ck-sidebar">
     <p class="ck-no-select"><?= Yii::t('ckimage', 'No image selected.') ?></p>
