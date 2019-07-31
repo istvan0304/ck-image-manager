164a8c3e7153343f4b49564685043e73715da22c
SZIUABP.PTE <szalai.istvan@pte.hu>
2019-07-31 14:21:35 +0200

    translations

--- messages/hu/ckimage.php
+++ messages/hu/ckimage.php
@@ -4,9 +4,12 @@ return [
     'Details' => 'Részletek',
     'File has been uploaded successfully!' => 'Fájl sikeresen feltöltve! ',
     'No image selected.' => 'Nincs kiválasztva fájl.',
+    'Nothing found.' => 'Nem található fájl.',
+    'Page not found!' => 'Az oldal nem található!',
     'Search...' => 'Keresés...',
     'Select' => 'Kiválasztás',
     'This file is already uploaded!' => 'Ezt a fájlt már feltöltötte!',
     'Upload' => 'Feltöltés',
-    'Uploaded: ' => 'Feltöltve: '
+    'Uploaded: ' => 'Feltöltve: ',
+    'Upload images to the server' => 'Képek feltöltése a szerverre'
 ];
